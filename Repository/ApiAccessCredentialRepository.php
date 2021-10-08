7f4f9e0ce9de3141c804cda56353ff37e1cfa529
Vipin Shrivastava <vipin.shrivastava889@webkul.com>
2021-10-08 16:01:46 +0530

    updates for php-8

--- Repository/ApiAccessCredentialRepository.php
+++ Repository/ApiAccessCredentialRepository.php
@@ -4,7 +4,7 @@ namespace Webkul\UVDesk\ApiBundle\Repository;
 
 use Webkul\UVDesk\ApiBundle\Entity\ApiAccessCredential;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method ApiAccessCredential|null find($id, $lockMode = null, $lockVersion = null)
